378404e0c6b3ae11911340ef195443c291382336
Andrew Tamburino <andytamburino@gmail.com>
2024-09-27 23:03:54 -0400

    added what I have created so far in git

---
+++ functions.php
@@ -0,0 +1,12 @@
+<?php 
+
+function university_files() {
+    wp_enqueue_script('main-uni-js', get_theme_file_uri('/build/index.js'), array('jquery'), '1.0', true);
+    wp_enqueue_style('uni_main_styles', get_theme_file_uri('/build/style-index.css'));
+    wp_enqueue_style('uni_extra_styles', get_theme_file_uri('/build/index.css'));
+    wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+    wp_enqueue_style('custom-google-fonts', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+
+}
+
+add_action('wp_enqueue_scripts', 'university_files');
