5db7a98d4073217e8099af017159fc3c9d9de3ac
Andrew Tamburino <57277787+atamburino@users.noreply.github.com>
2024-10-06 22:34:09 -0400

    completed part 35

--- functions.php
+++ functions.php
@@ -21,6 +21,23 @@ function uni_features(){
 add_action('after_setup_theme', 'uni_features');
 
 
+function uni_adjust_queries($query) {
+    if (!is_admin() AND is_post_type_archive('event') AND $query->is_main_query()) {
+        $today = date('Ymd');
+        $query->set('meta_key', 'event_date');
+        $query->set('orderby', 'meta_value_num');
+        $query->set('order', 'ASC');
+        $query->set('meta_query',array(
+            array(
+              'key' => 'event_date',
+              'compare' => '>=',
+              'value' => $today,
+              'type' => 'numeric'
+            )
+          ));
+    }
+}
+
+add_action('pre_get_posts', 'uni_adjust_queries');
 // added and created a new folder for the events post type this is in a new local folder
 // This folder is not on Github
-
